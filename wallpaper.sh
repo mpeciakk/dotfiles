797923aae2a55fe79a3bd49ce41a18ff1ce45f44
mpeciakk <mpeciakk@gmail.com>
2025-12-20 22:11:40 +0100

    update

--- wallpaper.sh
+++
@@ -1,4 +0,0 @@
-for monitor in $(hyprctl monitors | grep 'Monitor' | awk '{ print $2 }'); do
-  hyprctl hyprpaper preload "$HOME/dotfiles/wp.png"
-  hyprctl hyprpaper wallpaper "$monitor,$HOME/dotfiles/wp.png"
-done
